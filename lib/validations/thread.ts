7663d13dd544d96a68dc11c719ec87df67143656
ivanbarron2222 <ivanbarron903@gmail.com>
2024-11-26 22:09:24 +0800

    first commit

---
+++ lib/validations/thread.ts
@@ -0,0 +1,13 @@
+import * as z from "zod";
+
+export const ThreadValidation = z.object({
+  thread: z.string().min(1).min(3, { message: "Minimum of 3 characters" }),
+  accountId: z.string(),
+
+});
+
+
+export const CommentValidation = z.object({
+    thread: z.string().min(1).min(3, { message: "Minimum of 3 characters" }),
+  });
+  
